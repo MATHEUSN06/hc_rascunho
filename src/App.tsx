b0af44323419ab751d41659112df3fa4b983194e
MATHEUSN06 <MATHEUSNCORREGIO@GMAIL.COM>
2025-09-30 23:33:12 -0300

    erro tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,19 @@
-import { lazy, Suspense, useCallback, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Fallback } from "./components/fallback";
-import { Layout } from "./components/layout";
-import { Loading } from "./components/loading";
-import type { Workout } from "./types/workout";
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './components/AppRoutes';
+import Fallback from "./components/Fallback";
+import Layout from "./components/Layout";
 
-const Home = lazy(() =>
-  import("./pages/home").then((m) => ({ default: m.Home }))
-);
-
-const NotFound = lazy(() =>
-  import("./pages/not-found").then((m) => ({ default: m.NotFound }))
-);
-
-const AddWorkout = lazy(() =>
-  import("./pages/add-hc").then((m) => ({ default: m.AddWorkout }))
-);
-
-const WorkoutDetails = lazy(() =>
-  import("./pages/hc-details").then((m) => ({ default: m.WorkoutDetails }))
-);
-
-function App() {
-  const [workouts, setWorkouts] = useState<Workout[]>([]);
-
-  const removeWorkout = useCallback((id: string) => {
-    const workoutToDelete = workouts.findIndex((value) => {
-      return value.id === id;
-    });
-
-    const updatedWorkouts = [...workouts];
-
-    updatedWorkouts.splice(workoutToDelete, 1);
-
-    setWorkouts(updatedWorkouts);
-  }, []);
-
-  // Renderiza somente uma vez
-  const addWorkout = useCallback((workout: Workout) => {
-    setWorkouts((prev) => [...prev, workout]);
-  }, []);
-
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <ErrorBoundary FallbackComponent={Fallback}>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route
-                index
-                element={
-                  <Home workouts={workouts} removeWorkouts={removeWorkout} />
-                }
-              />
-              <Route
-                path="/add"
-                element={<AddWorkout onAdd={addWorkout} workouts={workouts} />}
-              />
-              <Route path="/workout/:id" element={<WorkoutDetails />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
+      <Layout>
+        <ErrorBoundary fallback={<Fallback />}>
+          <AppRoutes />
+        </ErrorBoundary>
+      </Layout>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
